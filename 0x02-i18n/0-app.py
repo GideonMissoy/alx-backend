d27efa8767c1c1add78d77351f41413103abbad6
GideonMissoy <giddykips2030@gmail.com>
2024-07-03 05:21:15 +0300

    Flask app.

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""0. Basic Flask app
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """hello world"""
+
+    return render_template('0-index.html')
